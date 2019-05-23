02e290d61799fe40a9c1298bf6ac933efd05d53a
Matthew Lam <matthew.lam@conversocial.com>
2019-05-23 17:39:16 +0100

    Finished up stage 2. Fixed a problem with key press event (for 'enter' key), added scrolling for chat window and disabled input forms if a 'chat hasn't been initialised'.

--- src/components/textBoxInput.js
+++ src/components/textBoxInput.js
@@ -11,26 +11,31 @@ class TextBoxInput extends React.Component {
     handleKeyPress(event) {
         if(event.key === 'Enter'){
             let inputText = document.getElementById("input_id").value;
-            this.props.getState(); // This doesn't work.
+            // this.props.getState(); // This doesn't work.
             this.handleOnClick(inputText) // This also doesn't work.
             console.log(inputText); // This works.
         }
-    }
-    
+    }    
 
 	render() {
-		return(    
+		return( 
             <React.Fragment>
                 <form id = "input_form" onSubmit = {(event) => event.preventDefault()}>
-                    <input type = {"text"} id = {"input_id"} defaultValue = {""} onKeyPress = {this.handleKeyPress}/>
+                    
+                    <input type = {"text"} id = {"input_id"} defaultValue = {""} 
+                    onKeyPress = {(event) => this.handleKeyPress(event)}
+                    disabled = {this.props.messages.length === 0 ? true: false}
+                    />
+
                     <input type = {"button"} value = {"Submit"} id = {"submit_button"} 
                     onClick = {() => this.handleOnClick(document.getElementById("input_id").value)} 
+                    disabled = {this.props.messages.length === 0 ? true: false} 
                 />
+
                 </form>
             </React.Fragment>
 		)
     }
-    
 }
 
 export default TextBoxInput;
